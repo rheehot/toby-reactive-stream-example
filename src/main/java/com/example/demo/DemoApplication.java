fb281613b3a32c29f131ea96cdb4b5b75acbacee
Geonguk Han <umanking@gmail.com>
2020-10-08 18:56:33 +0900

    feat: 06 asyncRestTemplate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,54 @@
 package com.example.demo;
 
+import io.netty.channel.nio.NioEventLoopGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import javax.print.DocFlavor;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @SpringBootApplication
 @EnableAsync
 public class DemoApplication {
+
+
+    @RestController
+    public static class MyController {
+
+        //        private WebClient client = WebClient.create();
+        // todo: better for WebClient example
+        private AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
+
+        static final String URL1 = "http://localhost:8081/service?req={req}";
+        static final String URL2 = "http://localhost:8081/service2?req={req}";
+
+
+        @GetMapping("/rest")
+
+        public DeferredResult<String> rest(int idx) {
+            final DeferredResult<String> dr = new DeferredResult<>();
+            Completion.from(rt.getForEntity(URL1, String.class, "hello" + idx))
+                    .andApply(s -> rt.getForEntity(URL2, String.class, s.getBody()))
+                    .andAccept(s -> dr.setResult(s.getBody()));
+
+            return dr;
+
+        }
+    }
 
     @Service
     public static class MyService {
@@ -24,6 +59,33 @@ public class DemoApplication {
     }
 
     public static class Completion {
+        private Completion next;
+        private Consumer<ResponseEntity<String>> con;
+        private Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn;
+
+        public Completion() {
+        }
+
+        public Completion(Consumer<ResponseEntity<String>> con) {
+            this.con = con;
+        }
+
+        public Completion(Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn) {
+            this.fn = fn;
+        }
+
+
+        public void andAccept(Consumer<ResponseEntity<String>> con) {
+            Completion c = new Completion(con);
+            this.next = c;
+        }
+        public Completion andApply(Function<ResponseEntity<String>, ListenableFuture<ResponseEntity<String>>> fn) {
+            Completion c = new Completion(fn);
+            this.next = c;
+
+            return c;
+        }
+
         public static Completion from(ListenableFuture<ResponseEntity<String>> lf) {
             final Completion completion = new Completion();
             lf.addCallback(
@@ -43,6 +105,15 @@ public class DemoApplication {
         }
 
         private void complete(ResponseEntity<String> s) {
+            if (next != null) next.run(s);
+        }
+
+        private void run(ResponseEntity<String> value) {
+            if (con != null) con.accept(value);
+            else if (fn != null) {
+                final ListenableFuture<ResponseEntity<String>> lf = fn.apply(value);
+                lf.addCallback(s -> complete(s), e -> error(e));
+            }
 
         }
     }
---
+++ src/main/java/com/example/demo/RemoteService.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Geonguk Han
+ * @since 2020-10-08
+ */
+@SpringBootApplication
+public class RemoteService {
+
+    @RestController
+    public static class MyController {
+
+        @GetMapping("/service")
+        public String service(String req) throws InterruptedException {
+            Thread.sleep(1000);
+
+            return req + "/service1";
+        }
+
+        @GetMapping("/service2")
+        public String service2(String req) throws InterruptedException {
+            Thread.sleep(1000);
+
+            return req + "/service2";
+        }
+    }
+}
